d52f08bfc355c4b822dd63d58a49c44607f060ee
up1 <somkiat.p@gmail.com>
2022-11-17 00:54:57 +0700

    Demo with data

---
+++ demo_data/src/test/java/com/example/demo_data/DemoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
