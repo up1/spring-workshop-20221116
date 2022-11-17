97dbc575e716f7c950bf4d2379791dc64f6950ed
up1 <somkiat.p@gmail.com>
2022-11-17 17:00:36 +0700

    Testing with response

--- day2_tx/src/main/java/com/somkiat/day2_tx/users/UserControllerAdvice.java
+++ day2_tx/src/main/java/com/somkiat/day2_tx/users/UserControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.somkiat.day2_tx.users;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,11 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class UserControllerAdvice {
 
     @ExceptionHandler(BusinessException.class)
-    public Error doWithError(BusinessException e){
+    public ResponseEntity<Error> doWithError(BusinessException e){
         Error error = new Error();
         error.setCode(e.id);
         error.setMessage("User id="+ e.id + " not found");
-        return error;
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
 }
